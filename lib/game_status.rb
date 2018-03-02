3d7871860880b229716012d84834fe5fc53e4a8f
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-02 05:20:29 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,6 +21,7 @@ def won?(board)
     win_index_3 = win_combination[2]
 
     positions = [board[win_index_1],board[win_index_2],board[win_index_3]]
-      positions.all? {|position| position == positions[0]}
+    positions.all? {|position| position_taken?(positions,position)}
+    positions.all? {|position| position == positions[0]}
   end
 end
